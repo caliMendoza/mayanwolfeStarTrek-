60ed44cb9f0dc91da13247da6dfe1562feb84696
calimendoza <mbaivictor1@gmail.com>
2022-06-03 08:21:04 -0700

    start-trek-project complete

--- MayanStarTrek/main.js
+++ MayanStarTrek/main.js
@@ -1,5 +1,5 @@
 
-document.querySelector('button').addEventListener('click', apiRequest)
+document.querySelector('#getButton').addEventListener('click', apiRequest)
 
 async function apiRequest(){
 
@@ -9,6 +9,17 @@ async function apiRequest(){
     const response = await fetch(`https://mayanwolfe-startrek-api.herokuapp.com/api/${alienName}`)
     const data = await response.json()
     console.log(data)
+
+    document.getElementById('alienName').innerText = data.speciesName
+    document.getElementById('alienWorld').innerText = data.homeworld
+    document.getElementById('alienFeatures').innerText = data.features
+    document.getElementById('alienFacts').innerText = data.interestingFact
+    document.getElementById('alienExamples').innerText = data.notableExamples
+    document.getElementById('alienImage').src = data.image
+    document.getElementById('alienCaption').innerText = data.speciesName
+
+
+
     } catch(error) {
 
         console.log(error)
